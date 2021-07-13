4e3d33d46b0e3688fe62e86058f917fb466bea66
Dtchmnt <dtchmnt.ishim@gmail.com>
2021-07-13 20:54:27 +0700

    create users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
+            $table->string('login');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('image');
+            $table->string('about');
+            $table->string('type');
+            $table->string('github');
+            $table->string('city');
+            $table->boolean('is_finished');
+            $table->string('phone');
+            $table->string('birthday');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
